4ba9b0918796e196e510b5a0b40ecacd38eacb5c
Daniil Sluysarev <mr.dancheak@gmail.com>
2023-12-05 12:57:16 +0300

    Fixed non-changing address on config load

--- internal/config/config.go
+++ internal/config/config.go
@@ -10,11 +10,11 @@ import (
 type Config struct {
 	Env         string `yaml:"env" env-default:"local"`
 	StoragePath string `yaml:"storage_path" env-required:"true"`
-	HTTPServer  `yaml:"http_server"`
+	HTTPServer  `yaml:"http_sever"`
 }
 
 type HTTPServer struct {
-	Address     string        `yaml:"address" env-default:"localhost:8080"`
+	Address     string        `yaml:"address" env-required:"true"`
 	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
 	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
 }
